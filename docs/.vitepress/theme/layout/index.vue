aa2e9324c8f2d70da3fb79028e852450aa44cf93
markthree <1801982702@qq.com>
2022-04-25 15:39:20 +0800

    chore: change link

--- docs/.vitepress/theme/layout/index.vue
+++ docs/.vitepress/theme/layout/index.vue
@@ -1,47 +1,54 @@
 <script setup>
-import { computed } from "vue";
+import { computed } from 'vue'
 import { useRoute, useRouter } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 const { Layout } = DefaultTheme
 const route = useRoute()
 const active = computed(() => {
-    const isGuide = route.path.includes('/guide')
-    return isGuide ? 'text-primary' : null
+	const isGuide = route.path.includes('/guide')
+	return isGuide ? 'text-primary' : null
 })
 
 const router = useRouter()
-const toLink = url => window.location.href = url
+const toLink = url => (window.location.href = url)
 </script>
 
 <template>
-    <Layout>
-        <template #navbar-search>
-            <div
-                :class="[active]"
-                class="cursor-pointer font-semibold text-lg active"
-                @click="router.go('/guide/')"
-            >Guide</div>
+	<Layout>
+		<template #navbar-search>
+			<div
+				:class="[active]"
+				class="cursor-pointer font-semibold text-lg active"
+				@click="router.go('/guide/')"
+				>Guide</div
+			>
 
-            <div
-                class="cursor-pointer font-semibold mx-4 text-lg active"
-                @click="toLink('https://github.com/markthree/tob-less')"
-            >Github</div>
+			<div
+				class="cursor-pointer font-semibold mx-4 text-lg active"
+				@click="
+					toLink('https://github.com/dishait/tob-less')
+				"
+				>Github</div
+			>
 
-            <div
-                class="cursor-pointer font-semibold text-lg active"
-                @click="toLink('https://gitee.com/markthree/tob-less')"
-            >Gitee</div>
-        </template>
-    </Layout>
+			<div
+				class="cursor-pointer font-semibold text-lg active"
+				@click="
+					toLink('https://gitee.com/dishait/tob-less')
+				"
+				>Gitee</div
+			>
+		</template>
+	</Layout>
 </template>
 
 <style scoped lang="less">
-@import (reference, less) "../custom.less";
+@import (reference, less) '../custom.less';
 .active {
-    transition: all 0.2s ease-in-out;
+	transition: all 0.2s ease-in-out;
 }
 
 .active:hover {
-    .text-primary;
+	.text-primary;
 }
 </style>
